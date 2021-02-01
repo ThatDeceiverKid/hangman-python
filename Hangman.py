644652dd5e674ee5ccf84db806b3ed14fe3902db
AlexBellune <38565926+ThatDeceiverKid@users.noreply.github.com>
2021-01-31 20:36:21 -0500

    Added checkChar functionality, not done, bugs found before taking a break listed in the To-Do list.

--- Hangman.py
+++ Hangman.py
@@ -2,6 +2,8 @@
 
 attemptsRemaining = 1
 playAgain = True
+blankList = []
+blanks = 0
 
 print("Welcome to Hangman!")
 
@@ -25,20 +27,51 @@ def init(userInt):
 
     return wordChoice
 
+#Checking the character choice against the word, and changed the blankList accordingly.
+
+def checkChar(char, blanks):
+    preBlank = blanks
+    counter = 0
+    for i in gameWord:
+        if gameWord[counter].upper() == char.upper():
+            blankList[counter] = char.upper()
+            blanks -= 1
+            counter += 1
+        else:
+            counter += 1
+    if blanks == preBlank:
+        return False
+    else:
+        return True
+
 while playAgain == True:
     print("Let's begin! \nPlease enter any integer.")
     wordChoiceInit = input()
     gameWord = init(wordChoiceInit)
+
+    blanks = len(gameWord)
+    for i in gameWord:
+        blankList.append("_") #Generating the blanks for each letter
+    print(*blankList)
+
     while attemptsRemaining != 0 & attemptsRemaining < 6:  #Tracks attempts, here the guessing loop is implemented
-        blankString = ""
-        blanks = len(gameWord)
-        for i in gameWord:
-            blankString += "_ " #Generating the blanks for each letter
-        print(blankString)
-        charChoice = input("Please pick a letter.\n")
+
+        #Had to do this twice when implementing this way so that in case the input is wrong initially, the prompt keeps asking.
+        charChoice = input("Please pick a single letter.\n")
+        print("Checking the word...")
+        while len(charChoice) != 1:
+            charChoice = input("Please pick a single letter.\n")
+            print("Checking the word...")
+        
+        if checkChar(charChoice,blanks) != True:
+            attemptsRemaining -= 1
+            print("Sorry, that letter is not in the word.")
+            
 
 #To-Do:
 # - Print the Hangman
 # - Implement the guessing system (attemptsRemaining, error checking user character choice, etc.)
+#       - Currently has an infinite loop when first letter is incorrect.
+#       - Loop only iterates letter selection once before it goes back to asking for an integer,
 # - Add a stop condition for the game (attemptsRemaining = 0, word guessed and they want to quit)
 # - *Eventually* create a stats upload system with a database (contained data TBD) for extra practice
